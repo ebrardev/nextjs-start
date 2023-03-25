12b39caad51172b7a86c18969d930239869152f4
Ebrar <16155052@mersin.edu.tr>
2023-03-25 12:11:51 +0300

    404 page

---
+++ pages/404.js
@@ -0,0 +1,11 @@
+
+function NotFoundPage() {
+    return(
+        <div>
+            <h1> 404</h1>
+    <h1>yok yok sayfa yok alooo</h1>;
+    </div>
+    )
+    }
+
+    export default NotFoundPage;
--- pages/portfolio/index.js
+++ pages/portfolio/index.js
@@ -1,7 +1,7 @@
 function Portfolio() {
     return (
     <div>
-    <h1>Portfolio</h1>;
+    <h1>Portfolio</h1>
     </div>
     )
 }
