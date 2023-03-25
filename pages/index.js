978eda958746eb42d8de00dd729088b6481dc9af
Ebrar <16155052@mersin.edu.tr>
2023-03-25 11:04:19 +0300

    Link

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
+ import Link  from "next/link";
  function HomePage() {
-    return <div>Welcome to Next.js!</div>
+    return <div>
+        <h1>The Home Page</h1>
+        <ul>
+            <li>
+                <Link href="/portfolio">Portfolio</Link>
+            </li>
+            <li>
+                <Link href="/clients">Clients</Link>
+            </li>
+            <li>
+                <Link href="/blog/sddf">Blog</Link>
+            </li>
+        </ul>
+    </div>
 }
 
 export default HomePage;
