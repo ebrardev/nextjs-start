ca84a884e1d2e919104ff48922087c90e2aae9a7
Ebrar <16155052@mersin.edu.tr>
2023-03-25 11:13:49 +0300

    navigating dynamic route

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,9 +1,18 @@
-function clientPage() {
+import Link from 'next/link'
+function ClientPage() {
     return (
         <div>
             <h1>The Client Page</h1>
+            <ul>
+                <li>
+                    <Link href="/clients/ebrar">Ebrar</Link>
+                </li>
+                <li>
+                    <Link href="/clients/tuna">tunanehri</Link>
+                </li>
+            </ul>
         </div>
     )
 }
 
-export default clientPage
+export default ClientPage
