693a9229f3fd954a8cc96f7dd77b6c0c221ec193
Ebrar <16155052@mersin.edu.tr>
2023-03-25 11:59:24 +0300

    Navigating programmatically

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -3,9 +3,19 @@ import { useRouter } from 'next/router'
 function ClientProjectPage() {
     const router = useRouter();
     console.log(router.query);
+
+    function loadProjectHandler() {
+        //load data...
+        // router.push('/clients/ebrar/projecta')
+        router.push({
+            pathname: '/clients/[id]/[clientprojectid]',
+            query: {id: 'ebrar', clientprojectid: 'projecta'}
+        })
+    }
     return (
         <div>
             <h1>anneni sevioysasn alkışla Client</h1>
+            <button onClick={loadProjectHandler} > Load a project A</button>
         </div>
     )
 }
